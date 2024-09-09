599cb9281d90d96aa728d04bc6973fae4c7650e6
Hicham <167074210+chicham04@users.noreply.github.com>
2024-09-09 15:51:31 +0200

    front-page

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main id="primary" class="site-main">
+<section class="banner">
+     <img src="<?php echo get_stylesheet_directory_uri() . '/images/nathalie-11.jpeg'; ?>" alt="header image" class="hero-header">
+     <div class="event-image">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/PHOTOGRAPHE EVENT.png'; ?>" alt="Photographe event image">
+    </div>
+</section>  
+
+<?php get_footer(); ?>
