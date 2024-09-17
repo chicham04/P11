f1cbffd309b9194c587c4cec923db706433b675a
Hicham <167074210+chicham04@users.noreply.github.com>
2024-09-17 11:22:41 +0200

    images + front-pages

--- front-page.php
+++ front-page.php
@@ -1,11 +1,16 @@
-<?php get_header(); ?>
+<?php
+// Template Name: Hero Template
+// Description: Modèle d'affichage de la section héros de la page d'accueil
 
-<main id="primary" class="site-main">
-<section class="banner">
-     <img src="<?php echo get_stylesheet_directory_uri() . '/images/nathalie-11.jpeg'; ?>" alt="header image" class="hero-header">
-     <div class="event-image">
-        <img src="<?php echo get_stylesheet_directory_uri() . '/images/PHOTOGRAPHE EVENT.png'; ?>" alt="Photographe event image">
+get_header();
+
+// Récupérer l'URL de l'image d'arrière-plan aléatoire
+$background_image_url = get_random_background_image();
+?>
+
+<section class="hero" style="background-image: url('<?php echo $background_image_url; ?>');">
+    <div class="hero__content">
+        <h1>Photographie Event</h1>
+        <!-- Autres éléments de la bannière héros -->
     </div>
-</section>  
-
-<?php get_footer(); ?>
+</section>
