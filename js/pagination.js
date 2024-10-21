1e2273b976773da4191c024343593e1178997cba
Hicham <167074210+chicham04@users.noreply.github.com>
2024-10-21 16:15:26 +0200

    Pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,17 +1,31 @@
-let currentPage = 1;
-$('#load-more').on('click', function() {
-  currentPage++; // Do currentPage + 1, because we want to load the next page
 
-  $.ajax({
-    type: 'POST',
-    url: '/wp-admin/admin-ajax.php',
-    dataType: 'html',
-    data: {
-      action: 'weichie_load_more',
-      paged: currentPage,
-    },
-    success: function (res) {
-      $('.publication-list').append(res);
-    }
+ /* Attache des événements aux images pour activer la lightbox lorsqu'elles sont cliquées. */
+
+function attachEventsToImages() {
+  console.log("Les photos se chargent");
+  const images = document.querySelectorAll(".icon-fullscreen");
+  images.forEach((image) => {
+    image.removeEventListener("click", imageClickHandler); 
+    image.addEventListener("click", imageClickHandler); 
   });
-});
+}
+
+//Traite la réponse AJAX pour ajouter des photos et mettre à jour l'interface.
+// @param {Object} response - La réponse du serveur.
+// @param {number} initialOffset - L'offset initial avant le chargement des nouvelles photos.
+
+function handleLoadResponse(response, initialOffset) {
+  console.log("Response from AJAX request: ", response);
+  if (response.success && response.data) {
+      appendPhotos(response.data.html);
+      const newOffset = initialOffset + 8; 
+      jQuery("#load-more-btn").data("offset", newOffset); 
+      attachEventsToImages(); 
+      if (!response.data.has_more_photos) {
+          handleNoPhotos();
+      }
+  } else {
+      handleNoPhotos();
+      console.error("Erreur: Aucune donnée reçue.");
+  }
+}
